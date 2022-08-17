52e3b88aba391eb0ff63f38323dd685f4d515c55
varakin <varakin_ivan@yahoo.com>
2022-08-18 02:32:25 +0300

    упражнения со звездочкой

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+/* первая домашка (19 номер урока)
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -19,3 +21,87 @@ personalMovieDB.movies[firstQuestionMovie] = firstQuestionMark;
 personalMovieDB.movies[secondQuestionMovie] = secondQuestionMark;
 
 console.log(personalMovieDB);
+
+*/
+
+/*вторая домашка
+
+// Место для первой задачи
+function firstTask() {
+    // Пишем решение вот тут
+    for (let i=5; i<=10; i++) {
+        console.log(i);
+    }
+    
+}
+
+// Место для второй задачи
+function secondTask() {
+    // Пишем решение вот тут
+    for (let i=20; i>=10; i--) {
+        console.log(i);
+        if (i === 14) break;
+    }
+    
+}
+
+// Место для третьей задачи
+function thirdTask() {
+    // Пишем решение вот тут
+    for (let i=2; i<=10; i=i+2) {
+        console.log(i);
+    }
+    
+}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+    // Пишем решение вот тут
+    let i = 1;
+    while (i<=14) {
+        i++;
+        if (i % 2 === 0) {
+            continue;
+         }  else {
+            console.log(i);
+         }
+    }    
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    // Пишем решение вот тут
+    for (let i=0; i<=5; i++) {
+        arrayOfNumbers[i]=i+5;
+    }
+    
+    // Не трогаем
+    return arrayOfNumbers;
+}
+*/
+
+
+/* третья домашка */
+
+
+const arrayOfNumbers = [];
+
+// Пишем решение вот тут
+for (let i=0; i<=5; i++) {
+    arrayOfNumbers[i]=i+5;
+}
+console.log(arrayOfNumbers);
