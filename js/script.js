9cb37a86ee578e1294bfeebdb469e3cda72e1919
varakin <varakin_ivan@yahoo.com>
2022-12-03 15:43:04 +0300

    exist 29 exersice

--- js/script.js
+++ js/script.js
@@ -98,10 +98,74 @@ function fifthTask() {
 /* третья домашка */
 
 
-const arrayOfNumbers = [];
+// const arrayOfNumbers = [];
 
-// Пишем решение вот тут
-for (let i=0; i<=5; i++) {
-    arrayOfNumbers[i]=i+5;
+// // Пишем решение вот тут
+// for (let i=0; i<=5; i++) {
+//     arrayOfNumbers[i]=i+5;
+// }
+// console.log(arrayOfNumbers);
+
+
+// // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// // Пишем решение вот тут
+// for (let i=0; arr.lenght;  i++){
+//     result[i] = arr[i]
+// }
+// // Не трогаем
+// console.log(result);
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     // Пишем решение вот тут
+// for (let i=0; i<data.length; i++) {
+//     if (typeof data[i] === "string") {
+//         data[i] = data[i] + ' - done';
+//     } else {
+//         data[i] = data[i]*2;
+//     }
+// }
+// console.log(data);
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result)
+
+// function sayHello(name) {
+//     return ('Привет, ' + name + '!');
+// }
+// console.log(sayHello('Anton'));
+
+// function returnNeighboringNumbers(x) {
+//     return [x-1,x,x+1];
+// }
+
+// console.log(returnNeighboringNumbers(5));
+
+function getMathResult(a, b) {
+    let tax = a;
+    let str = '';
+    if (typeof b === "number" && b>0) {
+        for (let i = 1; i < b; i++) {
+            str = str + tax + '---';
+            tax += a;
+            
+        }
+    } else {
+        return (a);
+    }
+    return str+tax
 }
-console.log(arrayOfNumbers);
+console.log(getMathResult(20, -5));
